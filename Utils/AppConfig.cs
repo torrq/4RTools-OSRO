476812040e16e73ef01945fdb1b246dcd363a914
torrq <120230227+torrq@users.noreply.github.com>
2025-04-22 22:14:00 -0700

    Relax the minimums

--- Utils/AppConfig.cs
+++ Utils/AppConfig.cs
@@ -20,16 +20,16 @@ namespace _4RTools.Utils
         public static string DebugLogFilePath = "4rtools_debug.log";
 
         // Delay defaults in milliseconds
-        public static int AutoPotDefaultDelay = 100; // Autopot Tab
-        public static int YggDefaultDelay = 100; // Yggdrasil tab
-        public static int SkillSpammerDefaultDelay = 100; // Skill Spammer tab
-        public static int AutoBuffSkillsDefaultDelay = 100; // Autobuff Skills tab
-        public static int AutoBuffItemsDefaultDelay = 100; // Autobuff Items tab
-        public static int ATKDEFSpammerDefaultDelay = 100; // ATK x DEF tab, spammer
-        public static int ATKDEFSwitchDefaultDelay = 100; // ATK x DEF tab, switch
-        public static int MacroDefaultDelay = 100; // default for Macro Song + Switch tabs
+        public static int AutoPotDefaultDelay = 50; // Autopot Tab
+        public static int YggDefaultDelay = 50; // Yggdrasil tab
+        public static int SkillSpammerDefaultDelay = 50; // Skill Spammer tab
+        public static int AutoBuffSkillsDefaultDelay = 50; // Autobuff Skills tab
+        public static int AutoBuffItemsDefaultDelay = 50; // Autobuff Items tab
+        public static int ATKDEFSpammerDefaultDelay = 50; // ATK x DEF tab, spammer
+        public static int ATKDEFSwitchDefaultDelay = 50; // ATK x DEF tab, switch
+        public static int MacroDefaultDelay = 50; // default for Macro Song + Switch tabs
         public static int SkillTimerDefaultDelay = 1000; // Skill Timer tab
-        public static decimal DefaultMinimumDelay = 15; // Default minimum delay for all tabs
+        public static decimal DefaultMinimumDelay = 10; // Default minimum delay for all tabs
 
         // Default colors
         public static Color DefaultButtonBackColor = Color.White;
